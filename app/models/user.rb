337093299e07bea40463f3db3e91fe9e5653c3a9
Unitato1 <robertsvihla@gmail.com.github.com>
2024-07-28 22:00:49 +0200

    Added username to sign up.

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,5 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  validates :username, presence: true, length: { minimum: 3, maximum: 20 }
 end
