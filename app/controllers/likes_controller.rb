83ffd27c8a076b96f26eaef0f4153e2b6b8204cb
Unitato1 <robertsvihla@gmail.com.github.com>
2024-07-30 12:02:53 +0200

    Added likes model and links to like the posts

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,8 +1,23 @@
 class LikesController < ApplicationController
   def create
+    @like = current_user.likes.build(post_id: params[:post_id])
+    if @like.save
+      flash[:notice] = "You liked the post."
+    else
+      flash[:notice] = "You alredy liked the post."
+    end
+    redirect_back(fallback_location: root_path)
   end
 
   def destroy
+    @like = current_user.likes.find_by(post_id: params[:id])
+    if !@like.nil? && @like.destroy
+      flash[:notice] = "You unliked the post."
+
+    else
+      flash[:notice] = "You alredy unliked the post."
+    end
+    redirect_back(fallback_location: root_path)
   end
 
   def update
